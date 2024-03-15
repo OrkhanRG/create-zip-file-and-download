ed06d8d5b96475809fc860d84b84d89cb7efaa92
OrkhaN <orxanismayilov851@gmail.com>
2024-03-16 01:07:40 +0400

    edit controller

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use ZipArchive;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
 
 class ZipController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        $zip = new ZipArchive;
+
+        $fileName = 'myNewFile.zip';
+
+        if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE)
+        {
+            $files = File::files(public_path('myFiles'));
+
+            foreach ($files as $key => $value) {
+                $relativeNameInZipFile = basename($value);
+                $zip->addFile($value, $relativeNameInZipFile);
+            }
+
+            $zip->close();
+        }
+
+        return response()->download(public_path($fileName));
+    }
 }
